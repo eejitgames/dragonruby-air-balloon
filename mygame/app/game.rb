9726adc33a3477c79c4fbcc6430135650bc5d6b3
eejit <eejitgames@gmail.com>
2024-11-17 20:09:17 +0000

    updates in progress

--- mygame/app/game.rb
+++ mygame/app/game.rb
@@ -3,10 +3,11 @@ class Game
 
   def tick
     defaults
-    outputs.background_color = [ 100, 100, 100 ]
+    outputs.background_color = [ 0x92, 0xcc, 0xf0 ]
     send(@current_scene)
 
     # outputs.debug.watch state
+    # outputs.watch "#{$gtk.current_framerate} FPS"
     outputs.debug.watch "room number: #{@room_number}"
     outputs.debug.watch "tick count: #{@clock}"
 
@@ -52,11 +53,15 @@ class Game
     if vector
       @vector_x = vector.x * @player.speed
       @vector_y = vector.y * @player.speed
-      @player_flip = true if @vector_x > 0
-      @player_flip = false if @vector_x < 0
+      @player_flip = false if @vector_x > 0
+      @player_flip = true if @vector_x < 0
     else
       @vector_x = 0
       @vector_y = 0
+    end
+
+    if inputs.keyboard.key_down.forward_slash
+      @room_number = (1023 * rand).to_i
     end
   end
 
@@ -65,74 +70,105 @@ class Game
 
     # update player x and y, also prevent player from going too far forward/back in the scene
     @player.x = (@player.x + @vector_x).cap_min_max(0, 1)
-    @player.y = (@player.y + @vector_y).cap_min_max(0.05, 1)
+    @player.y = (@player.y + @vector_y).cap_min_max(0, 1)
 
     @clock += 1
   end
 
   def render
     @render_items = []
+    @render_items << { x: 0, y: 0, w: 1280, h: 720, path: 'sprites/cloudy_background.png' }
+
     # draw_debug_grid
-    # draw_inner_wall_solids
+    draw_inner_walls
     draw_player
     outputs.primitives << @render_items
   end
 
   def draw_debug_grid
-    # @rows = 45 # 720 / 16
-    # @cols = 80 # 1280 / 16
-    @rows.times do |y|
-      @cols.times do |x|
-        @render_items << { x: x * 16 + 8, y: y * 16 + 8, w: 14, h: 14, path: :pixel, r: 200, g: 200, b: 200, anchor_x: 0.5, anchor_y: 0.5 }
+    3.times do |y|
+      4.times do |x|
+        @render_items << {
+          x: x * @section_width + @section_width/2,
+          y: y * @section_height + @section_height/2,
+          w: @section_width - 2,
+          h: @section_height - 2,
+          path: :pixel,
+          r: 200,
+          g: 200,
+          b: 200,
+          anchor_x: 0.5,
+          anchor_y: 0.5
+        }
       end
     end
   end
 
   # draw inner walls in room, forming a simple maze with wide corridors
-  def draw_inner_wall_solids
+  def draw_inner_walls
     @wall_seed = @room_number
-    draw_wall_segment_solids(x: 18, y: 30, dir: get_direction)
-    draw_wall_segment_solids(x: 33, y: 30, dir: get_direction)
-    draw_wall_segment_solids(x: 48, y: 30, dir: get_direction)
-    draw_wall_segment_solids(x: 63, y: 30, dir: get_direction)
-    draw_wall_segment_solids(x: 18, y: 17, dir: get_direction)
-    draw_wall_segment_solids(x: 33, y: 17, dir: get_direction)
-    draw_wall_segment_solids(x: 48, y: 17, dir: get_direction)
-    draw_wall_segment_solids(x: 63, y: 17, dir: get_direction)
+    draw_wall_segment(x: 1, y: 2, dir: get_direction)
+    draw_wall_segment(x: 2, y: 2, dir: get_direction)
+    draw_wall_segment(x: 3, y: 2, dir: get_direction)
+    draw_wall_segment(x: 1, y: 1, dir: get_direction)
+    draw_wall_segment(x: 2, y: 1, dir: get_direction)
+    draw_wall_segment(x: 3, y: 1, dir: get_direction)
   end
 
   # function to draw wall segments, pass in the x, y coordinates, and the direction to draw the segment
-  def draw_wall_segment_solids(x:, y:, dir:)
+  def draw_wall_segment(x:, y:, dir:)
     case dir
     when :N
-      @render_items <<  { x: (x - 1) * 16, y: (y - 1) * 16, w: 16, h: @segment_height, path: :pixel, r: 50, g: 50, b: 200 }
-      14.times do |i|
-        # align the room_grid array with what is presented on the screen
-        # @room_grid[ 45 - (y + i) ][ x - 1 ] = 1
-        @room_grid[ y - 1 + i ][ x - 1 ] = 1
-      end
+      xc = (x * @section_width - @wall_thickness / 2).to_i
+      yc = (y * @section_height - @wall_thickness + @wall_thickness / 2).to_i
+      wc = @wall_thickness
+      hc = @section_height + @wall_thickness
+      @render_items <<  {
+        x: xc,
+        y: yc,
+        w: wc,
+        h: hc,
+        path: 'sprites/vertical_cloud_wall.png',
+      }
     when :S
-      @render_items <<  { x: (x - 1) * 16, y: ((y - 1) * 16) - @segment_height + 16, w: 16, h: @segment_height, path: :pixel, r: 50, g: 50, b: 200 }
-      14.times do |i|
-        # @room_grid[ (45 + 13) - (y + i) ][ x - 1 ] = 1
-        @room_grid[ y + i - 14 ][ x - 1 ] = 1
-      end
+      xc = (x * @section_width - @wall_thickness / 2).to_i
+      yc = ((y - 1) * @section_height - @wall_thickness + @wall_thickness / 2).to_i
+      wc = @wall_thickness
+      hc = @section_height + @wall_thickness
+      @render_items <<  {
+        x: xc,
+        y: yc,
+        w: wc,
+        h: hc,
+        path: 'sprites/vertical_cloud_wall.png',
+      }
     when :E
-      @render_items <<  { x: (x - 1) * 16, y: (y - 1) * 16, w: @segment_width, h: 16, path: :pixel, r: 50, g: 50, b: 200 }
-      16.times do |i|
-        # @room_grid[ 45 - y ][ x + i - 1] = 1
-        @room_grid[ y - 1][ x + i - 1] = 1
-      end
+      xc = (x * @section_width - @wall_thickness / 2).to_i
+      yc = (y * @section_height - @wall_thickness / 2).to_i
+      wc = @section_width + @wall_thickness
+      hc = @wall_thickness
+      @render_items <<  {
+        x: xc,
+        y: yc,
+        w: wc,
+        h: hc,
+        path: 'sprites/horizontal_cloud_wall.png',
+      }
     when :W
-      @render_items <<  { x: ((x - 1) * 16) - @segment_width + 16, y: (y - 1) * 16, w: @segment_width, h: 16, path: :pixel, r: 50, g: 50, b: 200 }
-      16.times do |i|
-        # @room_grid[ 45 - y ][ x + i - 16] = 1
-        @room_grid[ y - 1][ x + i - 16] = 1
-      end
+      xc = ((x - 1) * @section_width - @wall_thickness / 2).to_i
+      yc = (y * @section_height - @wall_thickness / 2).to_i
+      wc = @section_width + @wall_thickness
+      hc = @wall_thickness
+      @render_items <<  {
+        x: xc,
+        y: yc,
+        w: wc,
+        h: hc,
+        path: 'sprites/horizontal_cloud_wall.png',
+      }
     end
   end
 
-  # this is a version of the generation system used in the arcade game berzerk - it follows the same patterns as the arcade game following a reset.
   def get_direction
     n1 = 0x7
     n2 = 0x3153
@@ -157,18 +193,18 @@ class Game
   end
 
   def draw_player
-    player_sprite_index = 0.frame_index(count: 6, tick_count_override: @clock, hold_for: 8, repeat: true)
-    @player_sprite_path = "sprites/misc/dragon-#{player_sprite_index}.png"
+    player_sprite_index = 0.frame_index(count: 4, tick_count_override: @clock, hold_for: 10, repeat: true)
+    @player_sprite_path = "sprites/balloon_#{player_sprite_index + 1}.png"
 
     @render_items << {
       x: x_to_screen(@player.x),
       y: y_to_screen(@player.y),
-      w: 128,
-      h: 128,
+      w: 120,
+      h: 176,
       anchor_x: 0.5,
       anchor_y: 0.5,
       path: @player_sprite_path,
-      flip_horizontally: false
+      flip_horizontally: @player_flip
     }
   end
 
@@ -200,30 +236,27 @@ class Game
     return if @defaults_set
     @lost_focus = true
     @clock = 0
-    @rows = 45 # 720 / 16
-    @cols = 80 # 1280 / 16
-    @room_grid = Array.new(@rows) { Array.new(@cols, 0) }
-    @segment_height = 16 * 12 + 2 * 16
-    @segment_width = 16 * 14 + 2 * 16
-    @room_number = 0x0153 # 339 decimal, 101010011 binary
-    @next_room_number = @room_number
+    @room_number = 0x0153 # 339 decimal
     @current_scene = :tick_title_scene
     @next_scene = nil
     @screen_height = 720
     @screen_width = 1280
+    @section_width = 320
+    @section_height = 240
+    @wall_thickness = 48
     @vector_x = 0
     @vector_y = 0
     @player = {
       x: 0.5,
-      y: 0.2,
+      y: 0.15,
       speed: 0.005,
     }
     audio[:music] = {
-      input: "sounds/InGameTheme20secGJ.mp3",
+      input: "sounds/InGameTheme20secGJ.ogg",
       x: 0.0,
       y: 0.0,
       z: 0.0,
-      gain: 0.5,
+      gain: 0.2,
       pitch: 1.0,
       paused: true,
       looping: true
